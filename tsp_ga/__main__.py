4fb5f958a755d7a4d4116349560b1770c38c5c53
gabteo <37905467+gabteo@users.noreply.github.com>
2021-10-20 17:55:21 -0300

    Opção de realizar múltiplas execuções; geração de boxplot

--- tsp_ga/__main__.py
+++ tsp_ga/__main__.py
@@ -1,4 +1,18 @@
 from tsp_ga import tsp_ga
+from cromossomo import cromossomo 
+from boxplot import boxplot
 
 if __name__ == '__main__':
-    tsp_ga.main()
+    execucoes = 1
+    somaDist = 0
+    melhorCrom = cromossomo.cromVazio()
+    melhoresDist = []
+    for i in range(execucoes):
+        melhorCrom = tsp_ga.main(tamPopulacao=100, nPais=25, geracoes=20, p=0.005)[0]
+        somaDist += melhorCrom.getDistTotal()
+        melhoresDist.append(melhorCrom.getDistTotal())
+    distTotalMedia = somaDist/execucoes
+
+    boxplot(melhoresDist)
+    print(distTotalMedia)
+    
